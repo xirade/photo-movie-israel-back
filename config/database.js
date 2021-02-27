e014bfb7ff7d2a865cb6490abbb34f436a855e55
xirade <6myusic6@gmail.com>
2021-02-27 20:18:46 +0200

    ready for deploy

--- config/database.js
+++ config/database.js
@@ -4,11 +4,19 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+        schema: env('DATABASE_SCHEMA', 'public'), // Not Required
+        ssl: {
+          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+        },
       },
       options: {
-        useNullAsDefault: true,
+        ssl: env.bool('DATABASE_SSL', false),
       },
     },
   },
